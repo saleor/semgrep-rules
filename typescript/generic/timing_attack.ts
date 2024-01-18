52b45dc3a1643cbcf5284ea7ba5d8f422d678def
Mikail Kocak <mikail.kocak@saleor.io>
2024-01-18 15:12:13 +0100

    [typescript] Add rule checking for time-safe comparison
    This adds a semgrep rule to detect potential unsafe comparison between input and the secrets.

---
+++ typescript/generic/timing_attack.ts
@@ -0,0 +1,37 @@
+import crypto from "crypto";
+
+let originalSignature = "...";
+let body = "...";
+
+const hash = crypto
+  .createHmac("sha512", "my-secret")
+  .update(body)
+  .digest("hex");
+
+const validSignature = `sha512=${hash.toUpperCase()}`;
+
+// Should detect when using !==, !=, ===, and ==.
+// ruleid: timing-attack-comparison
+if (validSignature !== originalSignature) {
+  throw new Error("The signature does not match");
+}
+// ruleid: timing-attack-comparison
+if (validSignature != originalSignature) {
+  throw new Error("The signature does not match");
+}
+// ruleid: timing-attack-comparison
+if (validSignature === originalSignature) {
+  throw new Error("The signature does not match");
+}
+// ruleid: timing-attack-comparison
+if (validSignature == originalSignature) {
+  throw new Error("The signature does not match");
+}
+
+// Should detect when storing the result into a variable.
+// ruleid: timing-attack-comparison
+var result = (validSignature === originalSignature);
+
+// It is valid to use crypto.timingSafeEqual().
+// ok: timing-attack-comparison
+crypto.timingSafeEqual(validSignature, originalSignature)
